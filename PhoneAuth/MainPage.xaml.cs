a02b6a16eac5d3c83ee149754d100f8eca05dcca
kenfong2021 <kfong1764@gmail.com>
2024-01-04 14:26:27 +0000

    Enchancement
    Remove the keys

--- PhoneAuth/MainPage.xaml.cs
+++ PhoneAuth/MainPage.xaml.cs
@@ -7,9 +7,7 @@ namespace PhoneAuth
     public partial class MainPage : ContentPage
     {
         private readonly IAuthenticationService _authenticationService;
-        private const string AndroidSiteKey = "6Le9okQpAAAAAFo_2iBb3pXY8QbnWnU47BGtnUGq";
-        private const string AndroidSecretKey = "6Le9okQpAAAAAJepuKL1OirK1arNKVwe2xragBxe";
-
+         
         public MainPage(IAuthenticationService authenticationService)
         {
             InitializeComponent();
@@ -19,33 +17,7 @@ namespace PhoneAuth
         private async void btnSubmit_Clicked(object sender, EventArgs e)
         {
             if (!string.IsNullOrEmpty(txtMobileNo.Text))
-            {
-
-                //var api = Android.Gms.SafetyNet.SafetyNetClass.GetClient(Platform.CurrentActivity);
-                //var response = await api.VerifyWithRecaptchaAsync(AndroidSiteKey);
-                //if (response != null && !string.IsNullOrEmpty(response.TokenResult))
-                //{
-                //    var captchaResponse = await ValidateCaptcha(response.TokenResult, AndroidSecretKey);
-                //    if (captchaResponse is null || !captchaResponse.Success)
-                //    {
-                //        await Toast.Make($"Invalid captcha: {string.Join(",", captchaResponse?.ErrorCodes ?? Enumerable.Empty<object>())}", ToastDuration.Long).Show();
-                //        return;
-                //    }
-
-                //    if (Platform.CurrentActivity!.PackageName != captchaResponse.ApkPackageName)
-                //    {
-                //        await Toast.Make($"Package Names do not match: {captchaResponse.ApkPackageName}", ToastDuration.Long).Show();
-                //    }
-                //    else
-                //    {
-                //        await Toast.Make("Success", ToastDuration.Long).Show();
-                //    }
-                //}
-                //else
-                //{
-                //    await Toast.Make("Failed", ToastDuration.Long).Show();
-                //}
-
+            { 
                 var isValidMobile = await _authenticationService.AuthenticateMobile(txtMobileNo.Text);
                 if (isValidMobile)
                 {
